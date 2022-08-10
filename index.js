068f2dadcef68df1ff6c15e47e57c154ff216ba3
sntgopl <105621110+sntgopl@users.noreply.github.com>
2022-08-10 13:08:38 -0300

    add imports to index and luxon date function

---
+++ index.js
@@ -0,0 +1,6 @@
+import '.javascript/book.js';
+import pageSelect from './javascript/spa';
+import showDate from './javascript/date';
+
+pageSelect();
+showDate();
--- javascript/spa.js
+++ javascript/spa.js
@@ -2,28 +2,27 @@ const navs = document.querySelectorAll('#navList > li');
 const sections = document.querySelectorAll('section');
 const addButton = document.querySelector('#addButton');
 
-navs.forEach((ele, index) => {
-  ele.addEventListener('click', () => {
+const pageSelect = () => {
+  navs.forEach((ele, index) => {
+    ele.addEventListener('click', () => {
+      sections.forEach((sec, i) => {
+        sec.classList.add('hidden');
+        navs[i].classList.remove('active');
+      });
+      ele.classList.add('active');
+      sections[index].classList.remove('hidden');
+    });
+  });
+
+
+  addButton.addEventListener('click', () => {
     sections.forEach((sec, i) => {
       sec.classList.add('hidden');
       navs[i].classList.remove('active');
     });
-    ele.classList.add('active');
-    sections[index].classList.remove('hidden');
+    sections[0].classList.remove('hidden');
+    navs[0].classList.add('active');
   });
-});
+}
 
-
-addButton.addEventListener('click', () => {
-  sections.forEach((sec, i) => {
-    sec.classList.add('hidden');
-    navs[i].classList.remove('active');
-  });
-  sections[0].classList.remove('hidden');
-  navs[0].classList.add('active');
-});
-
-const date = document.querySelector('#date');
-
-date.innerHTML = Date();
-
+export default pageSelect;
